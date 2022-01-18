ed52f2b3fcb8cadb15b55df0626f92bec0e66197
Lenar <linz14@mail.ru>
2022-01-18 15:04:13 +0300

    reviews links

--- resources/views/partials/reviews-aside.blade.php
+++ resources/views/partials/reviews-aside.blade.php
@@ -2,25 +2,25 @@
   <div class="reviews-block__item-title">Другие отзывы</div>
   <ul>
     <li>
-      <a href="https://yandex.ru/profile/89847807565" class="reviews-block__item-link" target="_blank">
+      <a href="@php the_field('yandex-maps',9) @endphp" class="reviews-block__item-link" target="_blank">
         <img src="@asset('images/review1.png')" alt="">
         <span>Отзывы на Яндекс-картах</span>
       </a>
     </li>
     <li>
-      <a href="https://uslugi.yandex.ru/search?action=addReview&profile=Vinipykh-1783745" class="reviews-block__item-link" target="_blank">
+      <a href="@php the_field('yandex-services',9) @endphp" class="reviews-block__item-link" target="_blank">
         <img src="@asset('images/review2.png')" alt="">
         <span>Отзывы на Яндекс-услугах</span>
       </a>
     </li>
     <li>
-      <a href="https://g.page/r/CcM-TRz5fGFwEAg/review" class="reviews-block__item-link" target="_blank">
+      <a href="@php the_field('google-maps',9) @endphp" class="reviews-block__item-link" target="_blank">
         <img src="@asset('images/review3.png')" alt="">
         <span>Отзывы на Google-картах</span>
       </a>
     </li>
     <li>
-      <a href="https://www.instagram.com/vini_smr" class="reviews-block__item-link" target="_blank">
+      <a href="https://www.instagram.com/@php the_field('instagram',9) @endphp" class="reviews-block__item-link" target="_blank">
         <img src="@asset('images/review4.png')" alt="">
         <span>Отзывы в Instagram</span>
       </a>
